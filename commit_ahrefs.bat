e1d6bbe4e91e76e73b8de27e440c3be7c3ed4f03
dextroussoftech <dexteroussoftech1@gmail.com>
2026-05-20 17:32:33 +0530

    Fix Ahrefs static crawl errors: Remove hardcoded canonical tags and shorten static default meta description in index.html

--- commit_ahrefs.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "Inject Ahrefs Webmaster Analytics tracking script into index.html head"
-git push origin main
---
+++ commit_static_seo.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Fix Ahrefs static crawl errors: Remove hardcoded canonical tags and shorten static default meta description in index.html"
+git push origin main
