3433f88d14447dacab1d347d5d8df7e390d73239
dextroussoftech <dexteroussoftech1@gmail.com>
2026-05-20 12:52:50 +0530

    Inject Ahrefs Webmaster Analytics tracking script into index.html head

---
+++ commit_ahrefs.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Inject Ahrefs Webmaster Analytics tracking script into index.html head"
+git push origin main
--- commit_analytics.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "Implement GA4 Conversion Tracking Events and B2B Campaign UTM Parameters to resolve Unassigned traffic"
-git push origin main
